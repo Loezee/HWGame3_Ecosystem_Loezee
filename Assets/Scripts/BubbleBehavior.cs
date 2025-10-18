f76aa69d93f1aa455cbb311625e13a2db72f01b2
Loezee <ll5669@nyu.edu>
2025-10-18 00:12:33 -0400

    tiny update

--- Assets/Scripts/BubbleBehavior.cs
+++ Assets/Scripts/BubbleBehavior.cs
@@ -2,17 +2,68 @@ using UnityEngine;
 
 public class BubbleBehavior : MonoBehaviour
 {
+    enum BubbleStates
+    {
+        bubble,
+        fish,
+        shark
+    }
 
+    [SerializeField]
+    float timeToFish;
+
+    [SerializeField]
+    GameObject Fish;
+
+    [SerializeField]
+    bool fishIsFood = false;
+
+    float timer;
+    BubbleStates state = BubbleStates.bubble;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
+    }
 
+    void OnEnable()
+    {
+        state = BubbleStates.bubble;
+        timer = 0f;
+
+        gameObject.tag = "food";
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (state != BubbleStates.bubble)
+        {
+            return;
+        } 
 
+        timer += Time.deltaTime;
+        if (timer >= timeToFish)
+        {
+            TransformIntoFish();
+        }
+    }
+
+    void TransformIntoFish()
+    {
+        state = BubbleStates.fish;
+
+        if (Fish != null)
+        {
+            var fish = Instantiate(Fish, transform.position, Quaternion.identity);
+            if (fishIsFood)
+            {
+                fish.tag = "food";
+            }
+
+        }
+
+        Destroy(gameObject); // destroy bubble
     }
 }
